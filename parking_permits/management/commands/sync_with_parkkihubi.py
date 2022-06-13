489aa8a25cfe2d9b4045106a5d8846a9e7f29e45
Aman Yadav <amanpdyadav@gmail.com>
2022-06-13 09:26:46 +0300

    Add management commands for automatic syncing
    Refs PV-428

---
+++ parking_permits/management/commands/sync_with_parkkihubi.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+
+from parking_permits.cron import automatic_syncing_of_permits_to_parkkihubi
+
+
+class Command(BaseCommand):
+    help = "Sync permits that has failed or hasn't been yet synced with parkkihubi."
+
+    def handle(self, *args, **options):
+        automatic_syncing_of_permits_to_parkkihubi()
