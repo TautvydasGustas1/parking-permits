048035e1791f4ccdfc5c5276b3fedf04c26b77b7
Mingfeng Yu <mfdevworld@gmail.com>
2022-06-16 10:47:32 +0300

    Remove unused DataExportForm
    It's replaced with object specific forms

--- parking_permits/forms.py
+++ parking_permits/forms.py
@@ -1,17 +1,13 @@
-import json
-
 from django import forms
 from django.contrib.postgres.forms import SimpleArrayField
 from django.db import models
 from django.db.models import Q
-from django.utils.translation import ugettext as _
 
 from parking_permits.models import Address, LowEmissionCriteria, Product
 from parking_permits.models.order import Order, OrderPaymentType
 from parking_permits.models.parking_permit import ParkingPermit, ParkingPermitStatus
 from parking_permits.models.refund import Refund, RefundStatus
 from parking_permits.paginator import QuerySetPaginator
-from parking_permits.utils import convert_to_snake_case
 
 EXPORT_DATA_TYPE_CHOICES = [
     ("permits", "Permits"),
@@ -25,62 +21,6 @@ PDF_EXPORT_DATA_TYPE_CHOICES = [
     ("order", "Order"),
     ("refund", "Refund"),
 ]
-
-
-class DataExportForm(forms.Form):
-    data_type = forms.ChoiceField(choices=EXPORT_DATA_TYPE_CHOICES)
-    order_by = forms.CharField(required=False)
-    search_items = forms.CharField(required=False)
-
-    def _validate_order_by(self, order_by):
-        return (
-            isinstance(order_by, dict)
-            and order_by.get("order_fields")
-            and order_by.get("order_direction")
-        )
-
-    def clean_order_by(self):
-        value = self.cleaned_data.get("order_by")
-        if not value:
-            return None
-        try:
-            order_by = json.loads(value)
-            converted_order_by = convert_to_snake_case(order_by)
-            if self._validate_order_by(converted_order_by):
-                return converted_order_by
-            else:
-                raise forms.ValidationError(_("Invalid order by"), code="invalid_data")
-        except json.JSONDecodeError:
-            raise forms.ValidationError(_("Invalid order by"), code="decode_error")
-
-    def _validate_search_item(self, search_item):
-        return (
-            isinstance(search_item, dict)
-            and search_item.get("match_type")
-            and search_item.get("fields")
-            and search_item.get("value")
-        )
-
-    def clean_search_items(self):
-        value = self.cleaned_data.get("search_items")
-        if not value:
-            return None
-        try:
-            search_items = json.loads(value)
-            converted_search_items = convert_to_snake_case(search_items)
-            if all(
-                [
-                    self._validate_search_item(search_item)
-                    for search_item in converted_search_items
-                ]
-            ):
-                return converted_search_items
-            else:
-                raise forms.ValidationError(
-                    _("Invalid search items"), code="invalid_data"
-                )
-        except json.JSONDecodeError:
-            raise forms.ValidationError(_("Invalid search items"), code="decode_error")
 
 
 class PdfExportForm(forms.Form):
--- parking_permits/tests/test_forms.py
+++ parking_permits/tests/test_forms.py
@@ -1,41 +1,6 @@
 from django.test import TestCase
 
-from parking_permits.forms import DataExportForm, PdfExportForm
-
-
-class DataExportFormTestCase(TestCase):
-    def test_form_is_valid_when_valid_data_provided(self):
-        data = {
-            "data_type": "orders",
-            "order_by": '{"order_fields": ["id"], "order_direction": "DESC"}',
-            "search_items": '[{"matchType": "exact", "fields": ["id"], "value": 1}]',
-        }
-        form = DataExportForm(data)
-        self.assertTrue(form.is_valid())
-
-    def test_form_not_valid_when_fail_to_decode_error(self):
-        data = {"data_type": "orders", "order_by": ";{}"}
-        form = DataExportForm(data)
-        self.assertFalse(form.is_valid())
-        self.assertTrue(form.has_error("order_by", code="decode_error"))
-
-    def test_form_not_valid_when_invalid_order_by_provided(self):
-        data = {"data_type": "orders", "order_by": '{"key": "value"}'}
-        form = DataExportForm(data)
-        self.assertFalse(form.is_valid())
-        self.assertTrue(form.has_error("order_by", code="invalid_data"))
-
-    def test_form_not_valid_when_fail_to_decode_search_items(self):
-        data = {"data_type": "orders", "search_items": "[,]"}
-        form = DataExportForm(data)
-        self.assertFalse(form.is_valid())
-        self.assertTrue(form.has_error("search_items", code="decode_error"))
-
-    def test_form_not_valid_when_invalid_search_items_provided(self):
-        data = {"data_type": "orders", "search_items": '[{"matchType": "exact"}]'}
-        form = DataExportForm(data)
-        self.assertFalse(form.is_valid())
-        self.assertTrue(form.has_error("search_items", code="invalid_data"))
+from parking_permits.forms import PdfExportForm
 
 
 class PdfExportFormTestCase(TestCase):
